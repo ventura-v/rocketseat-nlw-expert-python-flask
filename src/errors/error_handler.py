5ae22a8e1ee3fab20fbb637b2d575cca2aaa7251
Vinicius Ventura <vinicius__20@hotmail.com>
2024-02-12 17:11:44 -0300

    feat: Implementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,21 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        '''
+            exemplos de processos no tratamento dos erros
+                enviar para um log
+                enviar um e-mail de notificação
+        '''
+        return HttpResponse(
+            status_code=error.status_code,
+            body=[{
+                "title": error.name,
+                "detail": error.message
+            }]
+        )
+
     return HttpResponse(
         status_code=500,
         body={
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -7,6 +7,7 @@ from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tag_routes', __name__)
 
@@ -16,6 +17,7 @@ def create_tags():
     # definição de protocolo HTTP, troca de dados
     response = None
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
 
         http_request = HttpRequest(body=request.json)
