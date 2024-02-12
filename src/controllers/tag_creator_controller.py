8df8776810b3ddf276749534458200df7526f298
Vinicius Ventura <vinicius__20@hotmail.com>
2024-02-12 10:07:21 -0300

    feat: Implementing Barcode creator method

---
+++ src/controllers/tag_creator_controller.py
@@ -0,0 +1,35 @@
+from typing import Dict
+from src.drivers.barcode_handler import BarcodeHandler
+
+class TagCreatorController:
+    '''
+        responsabilidade para implementar as regras de negócio
+    '''
+
+    def create(
+            self,
+            product_code: str
+    ) -> Dict:
+        path_from_tag = self.__create_tag(product_code)
+        formatted_response = self.__format_response(path_from_tag)
+        return formatted_response
+
+    def __create_tag( # os 2 underlines (__) indica que é um método privado
+            self,
+            product_code: str
+    ):
+        barcode_handler = BarcodeHandler()
+        path_from_tag = barcode_handler.create_barcode(product_code)
+        return path_from_tag
+
+    def __format_response(
+            self,
+            path_from_tag: str
+    ) -> Dict:
+        return {
+            'data': {
+                'type': 'Tag Image',
+                'count': 1,
+                'path': f'{path_from_tag}.png'
+            }
+        }
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -8,11 +9,13 @@ class TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
         # métodos em classe precisa ter o "self"
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # lógica de regra de negócio
-        print('estou na minha view')
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
+
         # retorno HTTP
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
